2f6314e03e6cde33c674e1cd04f16b4e3c012d54
genesis <116662964+genefu@users.noreply.github.com>
2023-08-05 20:13:47 -0400

    changed home page texts

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,10 @@ import Image from "next/image"
 function Homepage() {
     return (
         <>
-            <title> genesis fu home </title>
+            <title> Genesis Fu </title>
             <h1 className = "text-black font-bold text-7xl text-center py-5"> HOME PAGE </h1>
-            <h1 className = "text-stone-600 font-bold text-xl text-center"> welcome to genesis fu's home page </h1>
-            <p className = "text-center pb-4"> it was very hard for me to get to this point of the website </p>
+            <h1 className = "text-stone-600 font-bold text-xl text-center"> Welcome to Genesis Fu&apos;s Home Page </h1>
+            <p className = "text-center pb-4"> This is still a huge work in progress. </p>
             <Image id = "img" className = "rounded-lg shadow-md" src = "/homepagepic.JPG" width = {300} height = {400} alt = "me and friends"/>
         </>
     )
