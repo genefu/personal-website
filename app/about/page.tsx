119398e341c42999702326c8ff5b7d7431e1e699
genesis <116662964+genefu@users.noreply.github.com>
2023-08-05 21:01:31 -0400

    corrected apostrophe

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,7 +8,7 @@ function Aboutpage() {
             <h1 className = "text-black font-bold text-7xl text-center py-5"> ABOUT ME </h1>
             <h1 className = "text-stone-600 font-bold text-xl text-center"> Hello, this page is all about me! </h1>
             <p className = "text-center"> This will have the link to my socials and ways to contact me. </p>
-            <p className = "text-center pb-4"> The picture below is of me and my friend&argos;s cat. </p>
+            <p className = "text-center pb-4"> The picture below is of me and my friend&apos;s cat. </p>
             <Image id = "img" className = "rounded-lg shadow-md" src = "/aboutmepic.jpg" width = {450} height = {700} alt = "me and a friend&argos;s cat"/> 
             <div className = "grid grid-cols-9">
                 <Link className = "col-start-4 col-span-1" href = "https://www.linkedin.com/in/genesis-fu-10b778249/" target = "_blank" rel = "noopener noreferrer">
