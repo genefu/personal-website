7f0381400ed72d2fe39887f02230d0a3aa1ad7a9
genesis <116662964+genefu@users.noreply.github.com>
2023-08-05 20:28:45 -0400

    changed img description
    on about me page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -8,7 +8,7 @@ function Aboutpage() {
             <h1 className = "text-black font-bold text-7xl text-center py-5"> ABOUT ME </h1>
             <h1 className = "text-stone-600 font-bold text-xl text-center"> hi this page is all about me </h1>
             <p className = "text-center pb-4"> this will have the links to my socials  to contact me and stuff about me </p>
-            <Image id = "img" className = "rounded-lg shadow-md" src = "/aboutmepic.jpg" width = {450} height = {700} alt = "me and friends"/> 
+            <Image id = "img" className = "rounded-lg shadow-md" src = "/aboutmepic.jpg" width = {450} height = {700} alt = "me and a friend&argos;s cat"/> 
             <div className = "grid grid-cols-9">
                 <Link className = "col-start-4 col-span-1" href = "https://www.linkedin.com/in/genesis-fu-10b778249/" target = "_blank" rel = "noopener noreferrer">
                     <Image className = "py-10" src = "/linkedin.PNG" width = {50} height = {100} alt = "linkedin profile"/> 
