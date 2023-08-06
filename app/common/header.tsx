6cd59850c9e0b4ab6a955180947de4d32d272fea
genesis <116662964+genefu@users.noreply.github.com>
2023-08-05 20:55:39 -0400

    active link color
    added code that changes color of link depending on which page user is currently on

--- app/common/header.tsx
+++ app/common/header.tsx
@@ -1,18 +1,21 @@
+'use client'
 import Link from "next/link"
+import { usePathname } from "next/navigation"
 
 export default function Header() {
+    const router = usePathname()
     return (
         <div>
             <nav className = "py-4 px-4">
                 <ul>
                     <li> 
-                        <Link href = "/"> genesis fu </Link>
+                        <Link className = {router == "/" ? "active" : ""} href = "/"> genesis fu </Link>
                     </li>
                     <li> 
-                        <Link href = "/about"> about me </Link>
+                        <Link className = {router == "/about" ? "active" : ""} href = "/about"> about me </Link>
                     </li>
                     <li>
-                        <Link href = "/projects"> my projects </Link>
+                        <Link className = {router == "/projects" ? "active" : ""} href = "/projects"> my projects </Link>
                     </li>
                 </ul>
             </nav>
